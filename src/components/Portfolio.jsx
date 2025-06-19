fd83073635d3da96e44589b965bbf2cb1b5ca395
lomaxanthony <122334835+lomaxanthony@users.noreply.github.com>
2025-06-18 20:20:07 -0700

    imporved a bit of styling on the portfolio section. Still needs A LOT of work.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,6 +6,7 @@ function Portfolio() {
         <div className="portfolioContainer">
           <div className="projectGrid">
             {projects.map(project => (
+              <div className="projectItem">
                 <PortfolioItem 
                   imgUrl={project.imgUrl}
                   title={project.title}
@@ -13,6 +14,7 @@ function Portfolio() {
                   github={project.github}
                   link={project.link}
                 />
+               </div> 
             ))}
           </div>  
         </div>
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -5,12 +5,15 @@ function PortfolioItem({title, imgUrl, stack, github, link}) {
             src={imgUrl}
           />
           <div>
-            <h3>{title}</h3>
-            <p>
+            <h2>{title}</h2>
+            <hr className="projectLineBreak"></hr>
+            <p className="stackType">
+              <h4>Stack:</h4>
               {stack.map((item, index) => (
                 <span key={index}>{item}</span>
               ))}
             </p>
+            <hr></hr>
             <a href={github} target="_blank" rel="noopener noreferrer">GitHub</a>
             <a href={link} target="_blank" rel="noopener noreferrer">Live Demo</a>
           </div>        
