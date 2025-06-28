1d009146e6cd934e9d11363d056131ba0c661f2f
lomaxanthony <122334835+lomaxanthony@users.noreply.github.com>
2025-06-28 12:00:24 -0700

    Small added dev note

--- src/data/noteEntries.js
+++ src/data/noteEntries.js
@@ -1,3 +1,18 @@
+/*  Entry Template
+
+    {
+        date:'',
+        title:'',
+        content: 
+        `        
+        `
+    }
+
+
+*/
+
+
+
 const noteEntries = [
     {
         date: '06/17/2025',
@@ -14,6 +29,13 @@ const noteEntries = [
         date:'05/27/2025',
         title:'test2',
         content:'Test from last month'
+    },
+    {
+        date:'06/28/2025',
+        title:'Focus',
+        content: 
+        `I need need need to only focus on one change at a time while I am doing development. I start to make a change, notice something that I don’t like, stop what I am doing to fix that—then continue to implement the new feature. It’s so silly. I just add so many variables to potentially debug when things don’t function properly. I know better after the fact, and I need to work on stopping the habit now while the code bases that I am working on are small (and mine).        
+        `
     }
 ]
 
